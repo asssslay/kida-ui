6d5464335e2207085c2d9188405b46fb88402f02
asssslay <avramova.ruslana.a.r.r@gmail.com>
2026-09-10 20:38:52 +0300

    Update Sticker Burst animation

--- apps/docs/src/demos/sticker-burst-basic.react.tsx
+++ apps/docs/src/demos/sticker-burst-basic.react.tsx
@@ -2,7 +2,7 @@ import { StickerBurst } from '@kida-ui/react'
 
 export default function StickerBurstBasic() {
   return (
-    <StickerBurst count={10} distance={72} stickers={['✦', '●', '♥', '★']}>
+    <StickerBurst count={10} distance={72} stickers={['✦', '🐈‍⬛', '♥', '★']}>
       <button type="button" className="demo-sticker-button">
         Make some joy
       </button>
